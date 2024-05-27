87817712d4e2e3b36ad477fbd58706e50a7c54ea
AhmadAlbara <ahmadalbara64@gmail.com>
2024-05-27 20:51:43 +0700

    add : Vercel Analytics

--- src/components/layouts/index.jsx
+++ src/components/layouts/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Sidebar from "./Sidebar";
 import Bottombar from "./Bottombar";
 import Footer from "./Footer";
-
+import { Analytics } from "@vercel/analytics/react";
 const Layouts = ({ children }) => {
   return (
     <div className="md:flex lg:m-auto lg:max-w-5xl lg:justify-center">
@@ -11,6 +11,7 @@ const Layouts = ({ children }) => {
         <main>{children}</main>
         <Bottombar />
         <Footer />
+        <Analytics />
       </div>
     </div>
   );
